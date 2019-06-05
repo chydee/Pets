48ea801f61f2f3f91708e79394db5b545e0c2289
Desmond <desmondmuqsit@yahoo.com>
2019-06-05 18:52:51 +0100

    error: app still cashes

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -61,8 +61,8 @@ public class PetCursorAdapter extends CursorAdapter {
         TextView summaryTextView = view.findViewById(R.id.summary);
 
         // Exrtract properties from cursor
-        String name = cursor.getString(cursor.getColumnIndexOrThrow(PetsEntry.COLUMN_NAME));
-        String summary = cursor.getString(cursor.getColumnIndexOrThrow(PetsEntry.COLUMN_BREED));
+        int name = cursor.getColumnIndex(PetsEntry.COLUMN_NAME);
+        int summary = cursor.getColumnIndex(PetsEntry.COLUMN_BREED);
 
         //Populate field with extracted properties
         nameTextView.setText(name);
