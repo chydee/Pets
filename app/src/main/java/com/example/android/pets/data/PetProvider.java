f8ad4802bf36a95d694a7a31b3dbcfca29f0aa68
Desmond <desmondmuqsit@yahoo.com>
2019-06-05 15:52:58 +0100

    update: delete() method updated

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -184,7 +184,22 @@ public class PetProvider extends ContentProvider {
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        //Get a writeable database
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case PETS:
+                //Delete all rows that match the selection and selectionArgs
+                return database.delete(PetsEntry.TABLE_NAME, selection, selectionArgs);
+            case PET_ID:
+                //Delete a single row given by the ID in the URI
+                selection = PetsEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                return database.delete(PetsEntry.TABLE_NAME, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Deletion not supported for " + uri);
+        }
     }
 
     /**
