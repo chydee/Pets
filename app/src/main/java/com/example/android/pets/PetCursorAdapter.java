505c37bb0964be60520b947a26563fed97f50232
Desmond <desmondmuqsit@yahoo.com>
2019-06-05 18:25:02 +0100

    update: bindView()

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,9 +2,13 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.pets.data.PetContract.PetsEntry;
 
 /**
  * {@link PetCursorAdapter} is an adapter for a list or grid view
@@ -36,7 +40,7 @@ public class PetCursorAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
     /**
@@ -51,6 +55,13 @@ public class PetCursorAdapter extends CursorAdapter {
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+
+        //Find fields to populate in inflated templates
+        TextView nameTextView = view.findViewById(R.id.name);
+        TextView summaryTextView = view.findViewById(R.id.summary);
+
+        // Exrtract properties from cursor
+        String name = cursor.getString(cursor.getColumnIndexOrThrow(PetsEntry.COLUMN_NAME));
 
     }
 }
